c69eca9cb0867c8741c9e5e3540d25054c6e5b7e
saurabhack <kamanesaurabh68@gmail.com>
2024-06-18 12:30:58 +0530

    sixth commit

--- sample.js
+++ sample.js
@@ -1,8 +1,18 @@
-let number=123
-let reverse=0;
-while(number!=0){
-    let r=number%10
-    reverse=reverse*10+r
-    number=parseInt(number/10)
-}
-console.log(reverse)
+let myform=document.querySelector("#myform")
+
+
+// let h3=document.createElement("h3")
+// let p=document.createElement("p")
+let submit=document.querySelector("#submit")
+myform.addEventListener('submit',function(e){
+    e.preventDefault();
+     let product=document.querySelector("#product").value;
+     let price=document.querySelector("#price").value;
+     let h3 = document.createElement("h3");
+     let p = document.createElement("p");
+     h3.innerHTML = product;
+     p.innerHTML = price;
+     let div=document.querySelector(".box") 
+     div.appendChild(h3);
+     div.appendChild(p);
+})
