c55df7b665b44d653a4f1e73d55a9cf480d432b9
saurabhack <kamanesaurabh68@gmail.com>
2024-06-23 23:03:09 +0530

    javascripts async project

--- palidrome.js
+++ palidrome.js
@@ -1,6 +1,21 @@
 function paludrome(num){
     let rev=0
+    let copy=num
     while(num!=0){
-        
+        let re=num%10
+        rev=rev*10+re
+        num=parseInt(num/10)
     }
+    if(copy==rev){
+        return 1
+    }else{
+        return -1
+    }
+
+}
+
+if(paludrome(121)==1){
+    console.log("palidrome")
+}else{
+    console.log("not palidrome")
 }
